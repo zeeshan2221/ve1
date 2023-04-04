248f07ebafa8040b050bcb99d75d0a2fc4b56e5f
zeeshan2221 <121931377+zeeshan2221@users.noreply.github.com>
2023-04-05 02:43:12 +0500

    Update ve1.py

--- ve1.py
+++ ve1.py
@@ -3,7 +3,7 @@ import json
 import os
 
 # Set up OpenAI API credentials
-api_key = 'sk-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'
+api_key = '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'
 headers = {
     'Content-Type': 'application/json',
     'Authorization': f'Bearer {api_key}'
